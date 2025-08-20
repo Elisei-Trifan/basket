e0a2aaa590eee42eec67b0bb86a7f55dd9fc5b97
trifan.e <trifan.e@dextechnology.com>
2025-08-20 13:26:12 +0300

    Практика

--- src/components/ex2.tsx
+++ src/components/ex2.tsx
@@ -1,4 +1,4 @@
-import React, { JSX, useState } from "react";
+import React, { JSX, useEffect, useState } from "react";
 import style from "./ex2.module.css";
 import { IoClose } from "react-icons/io5";
 
@@ -8,22 +8,36 @@ interface ITodo {
   done: boolean;
 }
 
-const preload = [
+const preload: ITodo[] = [
   { id: 1, text: "Выучить TS", done: false },
   { id: 2, text: "Выучить JS", done: false },
   { id: 3, text: "Выучить React", done: false },
 ];
 
 export const Ex2 = () => {
-  const [todo, setTodo] = useState<ITodo[]>(preload);
+  const [todo, setTodo] = useState<ITodo[]>(() => {
+    const saved = localStorage.getItem("todos");
+
+    if (saved) {
+      const parsed = JSON.parse(saved);
+      return parsed.length > 0 ? parsed : preload;
+    }
+    return preload;
+  });
   const [newTodo, setNewTodo] = useState("");
   const [filter, setFilter] = useState<"all" | "done" | "active">("all");
+  const [editingId, setEditingId] = useState<number | null>(null);
+  const [editingText, setEditingText] = useState("");
 
   const filteredTodo = todo.filter((todo) => {
     if (filter === "done") return todo.done;
     if (filter === "active") return !todo.done;
     return true;
   });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todo));
+  }, [todo]);
 
   const handleDeleteTodo = (id: number) => {
     setTodo((prev) => prev.filter((item) => id !== item.id));
@@ -36,6 +50,55 @@ export const Ex2 = () => {
       ),
     );
   };
+
+  const handleStartEditing = (id: number, currentText: string) => {
+    setEditingId(id);
+    setEditingText(currentText);
+  };
+
+  const handleSaveEditing = (id: number) => {
+    setTodo((prev) => {
+      return prev.map((item) =>
+        item.id === id
+          ? { ...item, text: editingText, done: !item.done }
+          : item,
+      );
+    });
+    setEditingId(null);
+    setEditingText("");
+  };
+
+  interface User {
+    id: number;
+    name: string;
+  }
+
+  function sayHello(this: User, age: number) {
+    console.log(`${this.name} is ${age}`);
+  }
+
+  const user = {
+    name: "Елисей",
+    id: 1,
+  };
+
+  sayHello.apply(user, [31]);
+
+  function A() {
+    {
+    }
+  }
+
+  function B() {
+    return a;
+  }
+
+  // @ts-ignore
+  let a = new A();
+  // @ts-ignore
+  let b = new B();
+
+  console.log(a === b);
 
   return (
     <div>
@@ -52,8 +115,9 @@ export const Ex2 = () => {
           if (!newTodo.trim()) return;
           setTodo((prev) => {
             const newTodoUp = newTodo[0].toUpperCase() + newTodo.slice(1);
-            const newId = Date.now();
-            return [...prev, { id: newId, text: newTodoUp, done: false }];
+            const newId =
+              prev.length > 0 ? Math.max(...prev.map((item) => item.id)) : 0;
+            return [...prev, { id: newId + 1, text: newTodoUp, done: false }];
           });
           setNewTodo("");
         }}
@@ -93,11 +157,27 @@ export const Ex2 = () => {
             checked={item.done}
             onChange={() => handleToggleCheck(item.id)}
           />
-          <p
-            className={`${style.container} ${item.done ? style.crossed : ""} `}
-          >
-            {item.id}. {item.text}
-          </p>
+
+          {editingId === item.id ? (
+            <input
+              type="text"
+              value={editingText}
+              onChange={(e) => setEditingText(e.target.value)}
+              onBlur={() => handleSaveEditing(item.id)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") return handleSaveEditing(item.id);
+                if (e.key === "Escape") return setEditingId(null);
+              }}
+              autoFocus
+            />
+          ) : (
+            <p
+              onDoubleClick={() => handleStartEditing(item.id, item.text)}
+              className={`${style.container} ${item.done ? style.crossed : ""} `}
+            >
+              {item.id}. {item.text}
+            </p>
+          )}
 
           <span>
             {
